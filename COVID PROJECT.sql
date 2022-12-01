3303d9e13c2f6ebc56eaf951c29fed41ebc1bce2
nahla khalifa <92749062+Nahlakhalifaa@users.noreply.github.com>
2022-11-30 22:38:49 -0600

    Add files via upload

--- COVID PROJECT.sql
+++ COVID PROJECT.sql
@@ -57,7 +57,69 @@ order by 1,2
  and de.date = vac.date
  where de.continent is not null
 
+ -- looking at total population vs vaccenations
+ select de.continent,de.location,de.date,de.population,vac.new_vaccinations from [Covid project]..['coviddeaths] as de
+ join [Covid project] .. ['covidvaccenations] AS vac
+ on de.location = vac.location
+ and de.date = vac.date
+ where de.continent is not null
+ order by 2,3
+
+ select de.continent,de.location,de.date,de.population,SUM(convert(int,vac.new_vaccinations)) over (partition by de.location order by de.location,de.date)as
+ RollingPeapleVaccenited--,(RollingPeapleVaccenited/population)*100
+ from [Covid project]..['coviddeaths] as de
+ join [Covid project] .. ['covidvaccenations] AS vac
+ on de.location = vac.location
+ and de.date = vac.date
+ where de.continent is not null
+ order by 2,3
+
+ --USE CTE
+ --WITH popvsvac (continent,location,Date,population,new_vaccinations,RollingPeapleVaccenited)
+ --as (
+  --select de.continent,de.location,de.date,de.population,vac.new_vaccinations,SUM(convert(int,vac.new_vaccinations)) over (partition by de.location order by de.location,de.date)a RollingPeapleVaccenited
+ --,(RollingPeapleVaccenited/population)*100from [Covid project]..['coviddeaths] as de
+ --join [Covid project] .. ['covidvaccenations] AS vac
+ --on de.location = vac.location
+-- and de.date = vac.date
+-- where de.continent is not null
+ --order by 3
+--)
+create table #precentpopulationvac
+(
+continent nvarchar(255),
+location nvarchar(255),
+date datetime,
+population numeric,
+new_vaccinations numeric,
+RollingPeapleVaccenited numeric
+)
+SET ANSI_WARNINGS OFF
+GO
+insert into #precentpopulationvac
+select de.continent,de.location,de.date,de.population,vac.new_vaccinations,SUM(CAST(vac.new_vaccinations AS bigint)) over (partition by de.location order by de.location,de.date)as
+ RollingPeapleVaccenited
+ from [Covid project]..['coviddeaths] as de
+ join [Covid project] .. ['covidvaccenations] AS vac
+   on de.location = vac.location
+   and de.date = vac.date
+ where de.continent is not null
+
+ select *,(RollingPeapleVaccenited/population)*100 from #precentpopulationvac
 
 
 
+       --create view
+create view precentpopulationvac as
+select de.continent,de.location,de.date,de.population,vac.new_vaccinations,SUM(convert(int,vac.new_vaccinations)) over (partition by de.location order by de.location,de.date)as
+ RollingPeapleVaccenited
+ --,(RollingPeapleVaccenited/population)*100
+ from [Covid project]..['coviddeaths] as de
+ join [Covid project] .. ['covidvaccenations] AS vac
+ on de.location = vac.location
+ and de.date = vac.date
+ where de.continent is not null
+ --order by 2,3
+
+ select * from precentpopulationvac
 
